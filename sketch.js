4a0135773c3f40fd30f283013965a400af8dc49e
cgannon04 <105737716+cgannon04@users.noreply.github.com>
2022-05-23 13:55:10 -0400

    Tornado files to display on GitHub

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800, 400);
+  
+}
+var x = 220;
+
+function draw() {
+  background(5,37,23);
+  ellipse (x,30, 400,70)
+  ellipse (x,70, 300,70)
+  ellipse (x, 100, 250, 50)
+  ellipse (x, 130, 200, 75)
+  ellipse (x, 160, 150, 50)
+  ellipse (x, 190, 100, 50)
+  ellipse (x, 220, 70, 30)
+  ellipse (x, 240, 40, 20)
+  ellipse (x, 245, 40, 10)
+  ellipse (x,250,40,5)
+  
+  x +=1
+  
+  fill (189, 190, 191)
+  stroke (189, 190, 191)
+  
+}
